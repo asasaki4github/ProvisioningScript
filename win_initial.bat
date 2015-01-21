153798b678ba9871cea52ec6c7df7140fa0e08fb
asasaki4github <asasaki4github@users.noreply.github.com>
2015-01-21 13:17:17 +0900

    Update win_initial.bat

--- win_initial.bat
+++ win_initial.bat
@@ -1,17 +1,3 @@
-@echo on
-
-REM Title Windowws Initial Provisinoing Script
-REM Date 2015/01/19
-REM Scripted by asasaki4github
-
-systeminfo > C:\systeminfo_result.txt
-ipconfig > C:¥ifconfig_result.txt
-
-REM Change ExecutionPolicy to RemoteSigned
+systeminfo > C:¥ifconfig_result.txt
 powershell Set-ExecutionPolicy RemoteSigned
-powershell Get-ChildItem > C:\Get-ChildItem.txt
-
-REM Execute PowerShell File (.ps1)
-powershell https://raw.githubusercontent.com/asasaki4github/ProvisioningScript/master/win_initial_setup.ps1
-
-REM Change ExecutionPolicy to Restricted
+powershell Get-ChildItem > C:¥Get-ChildItem.txt
