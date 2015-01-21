fa4398b810ba0b69f44734c6b7b1044cb348f136
asasaki4github <asasaki4github@users.noreply.github.com>
2015-01-21 19:31:15 +0900

    Update win_initial_setup.ps1

--- win_initial_setup.ps1
+++ win_initial_setup.ps1
@@ -2,4 +2,4 @@ $webclient = New-Object System.Net.WebClient
 $url = "https://download.mozilla.org/?product=firefox-28.0-SSL&os=win&lang=ja"
 $file = "$pwd/Firefox Setup 28.0.exe"
 $webclient.DownloadFile($url,$file)
-& '$pwd/Firefox Setup 28.0.exe' -ms
+& '$pwd/Firefox Setup 28.0.exe'
